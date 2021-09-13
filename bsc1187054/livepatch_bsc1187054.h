bc1b438507f988765d512f89f931fa35cf46a836
Nicolai Stange <nstange@suse.de>
2021-09-13 15:01:05 +0200

    Fix for CVE-2021-3573 ("Use-After-Free vulnerability in function hci_sock_bound_ioctl()")
    Live patch for CVE-2021-3573. Upstream commit e04480920d1e ("Bluetooth:
    defer cleanup of resources in hci_unregister_dev()").
    
    KLP: CVE-2021-3573
    References: bsc#1187054 CVE-2021-3573
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ bsc1187054/livepatch_bsc1187054.h
@@ -0,0 +1,24 @@
+#ifndef _LIVEPATCH_BSC1187054_H
+#define _LIVEPATCH_BSC1187054_H
+
+#if IS_ENABLED(CONFIG_BT)
+
+int livepatch_bsc1187054_init(void);
+void livepatch_bsc1187054_cleanup(void);
+
+
+struct hci_dev;
+struct socket;
+
+void klpp_hci_sock_dev_event(struct hci_dev *hdev, int event);
+int klpp_hci_sock_ioctl(struct socket *sock, unsigned int cmd,
+			  unsigned long arg);
+
+#else /* !IS_ENABLED(CONFIG_BT) */
+
+static inline int livepatch_bsc1187054_init(void) { return 0; }
+
+static inline void livepatch_bsc1187054_cleanup(void) {}
+
+#endif /* IS_ENABLED(CONFIG_BT) */
+#endif /* _LIVEPATCH_BSC1187054_H */
