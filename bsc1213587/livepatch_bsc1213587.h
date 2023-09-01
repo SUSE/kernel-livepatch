e4d63648fc674bb4458673bde21a0c1df6a04211
Marcos Paulo de Souza <mpdesouza@suse.com>
2023-09-01 12:05:41 -0300

    Fix for CVE-2023-3609 ("use-after-free in net/sched: cls_u32 can lead to local privilege escalation")
    Live patch for CVE-2023-3609. Upstream commit:
    - 04c55383fa56 ("net/sched: cls_u32: Fix reference counter leak leading to overflow")
    
    KLP: CVE-2023-3609
    References: bsc#1213587 CVE-2023-3609
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ bsc1213587/livepatch_bsc1213587.h
@@ -0,0 +1,19 @@
+#ifndef _LIVEPATCH_BSC1213587_H
+#define _LIVEPATCH_BSC1213587_H
+
+static inline int livepatch_bsc1213587_init(void) { return 0; }
+static inline void livepatch_bsc1213587_cleanup(void) {}
+
+struct net;
+struct tcf_proto;
+struct tc_u_knode;
+struct nlattr;
+struct netlink_ext_ack;
+
+int klpp_u32_set_parms(struct net *net, struct tcf_proto *tp,
+			 unsigned long base,
+			 struct tc_u_knode *n, struct nlattr **tb,
+			 struct nlattr *est, bool ovr,
+			 struct netlink_ext_ack *extack);
+
+#endif /* _LIVEPATCH_BSC1213587_H */
