62ba931bd3e71545ee3763868dd98f7601e8bafc
Marcos Paulo de Souza <mpdesouza@suse.com>
2024-06-14 16:34:18 -0300

    Fix for CVE-2024-26930 ("scsi: qla2xxx: double free of the ha->vp_map pointer")
    Live patch for CVE-2024-26930. Upstream commit:
    - e288285d4778 ("scsi: qla2xxx: Fix double free of the ha->vp_map pointer")
    
    KLP: CVE-2024-26930
    References: bsc#1223681 CVE-2024-26930
    Signed-off-by: Marcos Paulo de Souza <mpdesouza@suse.com>
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ bsc1223681/livepatch_bsc1223681.h
@@ -0,0 +1,24 @@
+#ifndef _LIVEPATCH_BSC1223681_H
+#define _LIVEPATCH_BSC1223681_H
+
+#if IS_ENABLED(CONFIG_SCSI_QLA_FC)
+
+int livepatch_bsc1223681_init(void);
+void livepatch_bsc1223681_cleanup(void);
+
+struct qla_hw_data;
+struct req_que;
+struct rsp_que;
+
+int
+klpp_qla2x00_mem_alloc(struct qla_hw_data *ha, uint16_t req_len, uint16_t rsp_len,
+	struct req_que **req, struct rsp_que **rsp);
+
+#else /* !IS_ENABLED(CONFIG_SCSI_QLA_FC) */
+
+static inline int livepatch_bsc1223681_init(void) { return 0; }
+static inline void livepatch_bsc1223681_cleanup(void) {}
+
+#endif /* IS_ENABLED(CONFIG_SCSI_QLA_FC) */
+
+#endif /* _LIVEPATCH_BSC1223681_H */
