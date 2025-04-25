8f07a2e577f7fc21774fbf8a937a757f11ac9af8
Miroslav Benes <mbenes@suse.cz>
2025-04-25 14:11:19 +0200

    uname_patch: Use klp-convert-mini instead of kallsyms
    Newer codestreams starting with SLE15-SP6 use klp-convert-mini
    infrastructure in the kernel. Convert uname_patch to use it instead of
    the current kallsyms.
    
    Signed-off-by: Miroslav Benes <mbenes@suse.cz>
    Reviewed-by: Nicolai Stange <nstange@suse.de>

--- uname_patch/livepatch_uname.h
+++ uname_patch/livepatch_uname.h
@@ -4,12 +4,6 @@
 #include <linux/utsname.h>
 #include "../klp_syscalls.h"
 
-#ifdef USE_KLP_CONVERT
-static inline int klp_patch_uname_init(void) { return 0; }
-#else
-extern int klp_patch_uname_init(void);
-#endif
-
 extern KLP_SYSCALL_DECLx(1, KLP_SYSCALL_SYM(klp_newuname),
 			 struct new_utsname __user *, name);
 
