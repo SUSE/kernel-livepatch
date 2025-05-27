5e0f43abc9074fe72cd99593d1f36e7e1116d189
Vincenzo Mezzela <vincenzo.mezzela@suse.com>
2025-05-27 10:57:13 +0200

    Fix for CVE-2024-57996 ("net_sched: sch_sfq: don't allow 1 packet limit")
    Live patch for CVE-2024-57996. Upstream commit:
    - 10685681bafc ("net_sched: sch_sfq: don't allow 1 packet limit")
    
    KLP: CVE-2024-57996
    References: bsc#1239077 CVE-2024-57996
    Signed-off-by: Vincenzo Mezzela <vincenzo.mezzela@suse.com>
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ bsc1239077/livepatch_bsc1239077.h
@@ -0,0 +1,11 @@
+#ifndef _LIVEPATCH_BSC1239077_H
+#define _LIVEPATCH_BSC1239077_H
+
+int livepatch_bsc1239077_init(void);
+void livepatch_bsc1239077_cleanup(void);
+
+int klpp_sfq_init(struct Qdisc *sch, struct nlattr *opt,
+                  struct netlink_ext_ack *extack);
+
+#endif /* _LIVEPATCH_BSC1239077_H */
+
