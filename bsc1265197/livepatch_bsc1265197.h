7323a195e251ec2a571f174e72d85c77fae4736a
Ali Abdallah <ali.abdallah@suse.de>
2026-07-03 10:08:40 +0200

    Fix for CVE-2026-43037 ("ip6_tunnel: clear skb2->cb[] in ip4ip6_err()")
    Live patch for CVE-2026-43037. Upstream commit:
    - 2edfa31769a4 ("ip6_tunnel: clear skb2->cb[] in ip4ip6_err()")
    
    KLP: CVE-2026-43037
    References: bsc#1265197 CVE-2026-43037
    Signed-off-by: Ali Abdallah <ali.abdallah@suse.de>
    Signed-off-by: Nicolai Stange <nstange@suse.de>

---
+++ bsc1265197/livepatch_bsc1265197.h
@@ -0,0 +1,14 @@
+#ifndef _LIVEPATCH_BSC1265197_H
+#define _LIVEPATCH_BSC1265197_H
+
+#include <linux/types.h>
+
+int livepatch_bsc1265197_init(void);
+void livepatch_bsc1265197_cleanup(void);
+
+
+struct inet6_skb_parm;
+struct sk_buff;
+
+int klpp_ip4ip6_err(struct sk_buff *skb, struct inet6_skb_parm *opt, u8 type, u8 code, int offset, __be32 info);
+#endif /* _LIVEPATCH_BSC1265197_H */
